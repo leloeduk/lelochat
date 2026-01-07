b0fa2605415590b99775cb4c973d3ee21fa6bda1
leloeduk <leloeduk2025@gmail.com>
2026-01-07 01:08:01 +0100

    feat(todo): add Todo states

---
+++ lib/features/todo/presentation/bloc/todo_state.dart
@@ -0,0 +1,15 @@
+part of 'todo_bloc.dart';
+
+abstract class TodoState {}
+
+class TodoLoading extends TodoState {}
+
+class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+  TodoLoaded(this.todos);
+}
+
+class TodoError extends TodoState {
+  final String message;
+  TodoError(this.message);
+}
