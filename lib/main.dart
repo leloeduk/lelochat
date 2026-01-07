d937b4d72fba3cd782aca891170d707bd5aa8788
leloeduk <leloeduk2025@gmail.com>
2026-01-07 01:24:54 +0100

    chore(todo): update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:lelochat/pages/data/expense_repository_impl.dart';
-import 'package:lelochat/pages/presentation/bloc/expense_bloc.dart';
-import 'package:lelochat/pages/presentation/pages/budget_page.dart';
+import 'features/todo/data/models/todo_model.dart';
+import 'features/todo/data/datasources/todo_local_data_source.dart';
+import 'features/todo/data/repositories/todo_repository_impl.dart';
+import 'features/todo/presentation/bloc/todo_bloc.dart';
+import 'features/todo/presentation/pages/todo_page.dart';
+import 'features/todo/domain/repositories/todo_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  final box = await Hive.openBox('expenses');
+  Hive.registerAdapter(TodoModelAdapter());
+  final box = await Hive.openBox<TodoModel>('todos');
+  final local = TodoLocalDataSourceImpl(box);
+  final repo = TodoRepositoryImpl(local);
 
-  runApp(MyApp(box));
+  runApp(MyApp(repo));
 }
 
 class MyApp extends StatelessWidget {
-  final Box box;
-  const MyApp(this.box, {super.key});
+  final TodoRepository repository;
+  const MyApp(this.repository, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) =>
-          ExpenseBloc(ExpenseRepositoryImpl(box))..add(LoadExpenses()),
+      create: (_) => TodoBloc(repository)..add(LoadTodos()),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: const Color(0xFF121212),
-          primaryColor: Colors.green,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color(0xFF121212),
-            elevation: 0,
-            centerTitle: true,
-          ),
-          colorScheme: const ColorScheme.dark(
-            primary: Colors.green,
-            secondary: Colors.redAccent,
-          ),
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white,
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: const Color(0xFF1E1E1E),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: BorderSide.none,
-            ),
-          ),
-        ),
-        home: const BudgetPage(),
+        home: const TodoPage(),
       ),
     );
   }
